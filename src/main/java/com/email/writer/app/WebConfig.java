9b14da065217e26fdb39c74327c490ba2c02bcf1
Movesh M <146848213+Moveshm@users.noreply.github.com>
2025-09-17 17:05:29 +0530

    Add files via upload

---
+++ src/main/java/com/email/writer/app/WebConfig.java
@@ -0,0 +1,22 @@
+package com.email.writer.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://mail.google.com") // safer, ONLY allow Gmail extension use!
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
